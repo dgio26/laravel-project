1e8a6e78bd3aced09a83bd2e67de708f9fdec58a
dgio26 <decyagiovanni26@gmail.com>
2024-09-12 10:11:17 +0700

    laravel-project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Welcome to My About Page!</h3>
+    <br>
+    <p>Name : {{ $name }}</p>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Welcome to My Contact Page!</h3>
+    <br>
+    <ul>
+        <li>
+            <a href="https://instagram.com/decya_gio">- Instagram</a>
+        </li>
+        <li>
+            <a href="https://x.com/dgiooo26">- X</a>
+        </li>
+    </ul>
+</x-layout>
